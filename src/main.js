48f224e6f71311167e542d4b134693dc1c412189
crop <bryce@mathx.ca>
2021-01-19 07:05:21 -0800

    Serve index.html from filesystem instead of express

--- src/main.js
+++ src/main.js
@@ -1,26 +1,10 @@
 'use strict'
-/**
- * Main script
- */
+
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path')
-const { app, BrowserWindow, dialog, Menu, protocol, ipcMain } = require('electron');
-const fs = require('fs');
+const fs = require('fs')
 
 let mainWindow
-
-
-ipcMain.on('asynchronous-message', (event, arg) => {
-    console.log(arg) // prints "ping"
-    event.reply('asynchronous-reply', 'pong')
-})
-
-ipcMain.on('synchronous-message', (event, arg) => {
-    console.log(arg) // prints "ping"
-    event.returnValue = 'pong'
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 
 function createWindow() {
     mainWindow = new BrowserWindow({
@@ -30,10 +14,10 @@ function createWindow() {
             nodeIntegration: true
         }
     })
-    mainWindow.loadURL('http://localhost:4340/')
-    mainWindow.webContents.openDevTools()
+    mainWindow.loadURL(path.join(__dirname, '../public/index.html'))
+    //mainWindow.webContents.openDevTools()
     function windowSend(a, b) { mainWindow.webContents.send(a, b) }
-    const template = [
+    const menuJ = [
         {
             label: 'File',
             submenu: [
@@ -60,16 +44,11 @@ function createWindow() {
             ]
         }
     ];
-    const menu = Menu.buildFromTemplate(template)
+    const menu = Menu.buildFromTemplate(menuJ)
     Menu.setApplicationMenu(menu)
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', () => {
-
-    Error.stackTraceLimit = Infinity
 
     require(path.join(__dirname, 'routes'))
     require(path.join(__dirname, 'websocketmgr'))
@@ -77,17 +56,11 @@ app.on('ready', () => {
 
     createWindow()
 
-
     app.on('activate', function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
 })
--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,7 @@
 'use strict'
 
-const path = require('path')
-const express = require('express')
-const app = express()
-const config = require(path.join(__dirname, 'config'))
-
 const routes = {
     'file': null
 }
-
-// output the required ws port number
-app.get('/wsport', function (req, res) {
-    res.send((config.port + 1).toString())
-})
-
-// whole public folder
-app.use(express.static(path.join(__dirname, '../public')))
-
-// create the server
-app.listen(config.port, config.host, function () {
-
-})
 
 module.exports = routes
